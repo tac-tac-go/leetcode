c0c4216b2eddf673e647c59a699a65f6762228b4
pypymi <dr.mizoguchi@gmail.com>
2021-05-14 15:11:39 +0900

    1207-unique-number-of-occurrences

---
+++ 1207-unique-number-of-occurrences.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    var counts= {}
+    for(let i=0;i<arr.length;i++){
+        var key = arr[i];
+        counts[key] = (counts[key]) ? counts[key]+1: 1
+    }
+    var counts2=new Array(1000)
+    counts2.fill(0);
+    for(const value in counts){
+       counts2[counts[value]]+=1;
+    }
+    var array_f = counts2.filter(v => {
+        return v>1
+    })
+    return array_f.length==0 ? true : false;
+};
