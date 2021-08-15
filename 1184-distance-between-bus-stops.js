d816e48a98a2f854d5973b14a7f1b3e51ba78eb1
pypymi <dr.mizoguchi@gmail.com>
2021-08-15 21:03:15 +0900

    test

---
+++ 1184-distance-between-bus-stops.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
+ */
+var distanceBetweenBusStops = function(distance, start, destination) {
+    let start1 = Math.min(start,destination)
+    let destination1 = Math.max(start,destination)
+    let anticlock = distance.concat(distance)
+    let value1 = 0
+    
+    for(let i=start1;i<destination1;i++){
+        value1+=anticlock[i]
+    }
+    let value2=0
+    for(let j=start1+distance.length-1;j>=destination1;j--){
+        value2+=anticlock[j]
+    }
+    return Math.min(value1,value2)
+
+};
