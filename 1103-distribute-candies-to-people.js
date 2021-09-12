cdcb71715e9194f05cd77cc7c9f36ec93cc09ae7
pypymi <dr.mizoguchi@gmail.com>
2021-09-12 10:30:14 +0900

    1103

---
+++ 1103-distribute-candies-to-people.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} candies
+ * @param {number} num_people
+ * @return {number[]}
+ */
+var distributeCandies = function(candies, num_people) {
+    let index=0;
+    let count=0;
+    let result = new Array(num_people).fill(0)
+    while(candies>count){
+        if(index+1>=candies-count){
+            result[index%num_people]+=candies-count
+            break   
+        }else{
+            result[index%num_people]+=index+1
+            index+=1
+            count+=index
+        }
+    }
+    return result
+};
