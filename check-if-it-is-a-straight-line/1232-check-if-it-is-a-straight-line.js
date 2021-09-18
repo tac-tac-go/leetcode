07c3781b916f641f4563be413799961ca9e9a3fc
pypymi <dr.mizoguchi@gmail.com>
2021-09-18 12:33:32 +0900

    test

---
+++ check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -0,0 +1,10 @@
+const checkStraightLine = (coordinates) => {
+  if (coordinates.length == 2) return true;
+  for (let i = 0; i < coordinates.length - 2; ++i)
+    if (
+      (coordinates[i + 2][1] - coordinates[i + 1][1]) * (coordinates[i + 1][0] - coordinates[i][0]) !=
+      (coordinates[i + 1][1] - coordinates[i][1]) * (coordinates[i + 2][0] - coordinates[i + 1][0])
+    )
+      return false;
+  return true;
+};
