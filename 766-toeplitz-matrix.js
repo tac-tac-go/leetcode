751e4a28c71cff0d3efc897938c92558171ba4cf
pypymi <dr.mizoguchi@gmail.com>
2021-10-15 17:52:05 +0900

    test

---
+++ 766-toeplitz-matrix.js
@@ -0,0 +1,12 @@
+var isToeplitzMatrix = function(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i+1] !== undefined && matrix[i+1][j+1] !== undefined) {
+        if(matrix[i][j] !== matrix[i+1][j+1]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
