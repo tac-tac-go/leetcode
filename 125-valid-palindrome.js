fdc19ab0d7d11c5cafe24cd167c6839a93da75ae
pypymi <dr.mizoguchi@gmail.com>
2021-10-17 22:07:18 +0900

    test

---
+++ 125-valid-palindrome.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+   var a = s.split("").reverse().join("").toLowerCase().replace(/[^a-z0-9]/gi,""); 
+   var b = s.toLowerCase().replace(/[^a-z0-9]/gi,"");
+    return a==b ? true : false;
+    
+};
