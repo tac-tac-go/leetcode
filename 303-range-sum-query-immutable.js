b75ff442203205ac8082be38bcbdef2f7c16196b
pypymi <dr.mizoguchi@gmail.com>
2021-10-23 08:56:41 +0900

    303

---
+++ 303-range-sum-query-immutable.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    let y = 0;
+    this.cumsum = nums.map(v => y+=v)
+    
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    console.log(left,right)
+    console.log(this.cumsum)
+    return this.cumsum[right] - (this.cumsum[left-1] || 0)
+    
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
