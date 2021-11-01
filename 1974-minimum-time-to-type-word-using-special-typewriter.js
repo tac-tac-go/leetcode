0a5f7daa08bb31f0aa6f97a1dc0945e417b845c0
pypymi <dr.mizoguchi@gmail.com>
2021-11-01 12:33:22 +0900

    1974

---
+++ 1974-minimum-time-to-type-word-using-special-typewriter.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minTimeToType = function(word) {
+    let s = 'abcdefghijklmnopqrstuvwxyz';
+    let count = 0;
+    let prev = 0;
+    
+    for (let i=0; i<word.length; i++) {
+        let index = s.indexOf(word[i]);
+        let moves = Math.abs(index-prev);
+        count += Math.min(moves, 26-moves)+1;
+        prev = index;
+    }
+    return count;
+};
