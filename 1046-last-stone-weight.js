758fa28c355dee5e397e87634bb410bee66b86f3
pypymi <dr.mizoguchi@gmail.com>
2021-11-04 23:55:53 +0900

    1046

---
+++ 1046-last-stone-weight.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    stones.sort((a,b) => b-a)
+    
+    while(stones.length>1){
+        console.log(stones)
+        if(stones[0]>stones[1]){
+            stones.push(stones[0]-stones[1])
+            stones = stones.slice(2)
+        }else{
+            stones = stones.slice(2)
+        }
+        stones.sort((a,b) => b-a)
+        
+    }
+    return stones
+};
