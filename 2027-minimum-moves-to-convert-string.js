794aada7414e78ba5277b6a56c7c6b946491ad07
pypymi <dr.mizoguchi@gmail.com>
2021-11-17 23:55:16 +0900

    test

---
+++ 2027-minimum-moves-to-convert-string.js
@@ -0,0 +1,10 @@
+var minimumMoves = function(s) {
+    let count = 0;
+    for( let i=0; i<s.length; i++ ) {
+        if( s[i] === 'X' ) {
+            count+= 1;
+            i+= 2;
+        }
+    }
+    return count;
+};
