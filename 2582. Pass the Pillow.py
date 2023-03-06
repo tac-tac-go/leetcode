91f9970fa867ba1daf72320cd6464410e3f861f8
pypymi <dr.mizoguchi@gmail.com>
2023-03-06 13:25:34 +0900

    test

---
+++ 2582. Pass the Pillow.py
@@ -0,0 +1,7 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        n-= 1                      
+        time%= n+n                  
+        return n+1 - abs(n - time)
+
+
