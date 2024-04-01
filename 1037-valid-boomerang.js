7c97193fa4d2171e2c9846c6a72c2ea1e263253c
pypymi <dr.mizoguchi@gmail.com>
2024-04-01 20:35:57 +0900

    test

--- 1037-valid-boomerang.js
+++ 1037-valid-boomerang.js
@@ -2,15 +2,15 @@
  * @param {number[][]} points
  * @return {boolean}
  */
-var isBoomerang = function(points) {
+let isBoomerang = function(points) {
     if(JSON.stringify(points[0])!=JSON.stringify(points[1]) && 
      JSON.stringify(points[0])!=JSON.stringify(points[2]) && 
      JSON.stringify(points[1])!=JSON.stringify(points[2])
     ){
         points.sort((a,b) => a[0]-b[0]);
         if((points[1][0]-points[0][0])==0 && (points[2][0]-points[1][0])==0){return false}
-        var xy_slope = (points[1][1]-points[0][1])/(points[1][0]-points[0][0])
-        var yz_slope = (points[2][1]-points[1][1])/(points[2][0]-points[1][0])
+        let xy_slope = (points[1][1]-points[0][1])/(points[1][0]-points[0][0])
+        let yz_slope = (points[2][1]-points[1][1])/(points[2][0]-points[1][0])
         return xy_slope!=yz_slope ? true : false;
     }
     return false
