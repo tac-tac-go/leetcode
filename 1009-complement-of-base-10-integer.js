cc2c8b5841f18cb9d84c55795ff579ccf7159fa5
pypymi <dr.mizoguchi@gmail.com>
2024-04-01 20:38:06 +0900

    test

--- 1009-complement-of-base-10-integer.js
+++ 1009-complement-of-base-10-integer.js
@@ -2,7 +2,7 @@
  * @param {number} N
  * @return {number}
  */
-var bitwiseComplement = function(N) {
+let bitwiseComplement = function(N) {
     let two_n = N.toString(2).split("");
     let two_n_r = two_n.map(n => n==1 ? 0 : 1).join('');
     return parseInt(two_n_r,2);
