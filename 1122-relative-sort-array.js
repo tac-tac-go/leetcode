2fc0310584fe84f170e44bd2f01457f5c7bbc358
pypymi <dr.mizoguchi@gmail.com>
2024-04-04 09:27:52 +0900

    test

--- 1122-relative-sort-array.js
+++ 1122-relative-sort-array.js
@@ -3,13 +3,13 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-var relativeSortArray = function(arr1, arr2) {
-    var result = []
+const relativeSortArray = function(arr1, arr2) {
+    let result = []
     for(let value of arr2){
         var array = arr1.filter(v => v===value)
         result = result.concat(array)
     }
-    var rest = arr1.filter(v => result.indexOf(v)==-1)
+    let rest = arr1.filter(v => result.indexOf(v)==-1)
     rest.sort((a,b) => a-b)
     result = result.concat(rest)
     return result
