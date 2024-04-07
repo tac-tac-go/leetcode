6febfbb5282bd085f93e047c2622f8bf46cb4b9e
pypymi <dr.mizoguchi@gmail.com>
2024-04-07 10:28:18 +0900

    test

--- 1154-day-of-the-year.js
+++ 1154-day-of-the-year.js
@@ -2,12 +2,12 @@
  * @param {string} date
  * @return {number}
  */
-var dayOfYear = function(date) {
-   var date_s = date.split("-")
-   var year = date_s[0]
-   var month = date_s[1]
-   var day = date_s[2]
-   var result=0
+let dayOfYear = function(date) {
+   let date_s = date.split("-")
+   let year = date_s[0]
+   let month = date_s[1]
+   let day = date_s[2]
+   let result=0
    for(let i=1;i<month;i++){
        result+=new Date(year,i, 0).getDate();
    }
