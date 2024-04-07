1316f0daf21d444a48393d322c6077acc5114e08
pypymi <dr.mizoguchi@gmail.com>
2024-04-07 10:27:44 +0900

    test

--- 1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number.js
@@ -2,16 +2,16 @@
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function(n) {
+let tribonacci = function(n) {
    const r_l = Array.from(Array(n+1), (v, k) => k)
-   var nums = r_l.map(cal);
+   let nums = r_l.map(cal);
    return nums.slice(-1)[0];
     
 };
 
-var cal = (function(){
-  var memoized = {};
-  var fn = function (it) {
+let cal = (function(){
+  let memoized = {};
+  let fn = function (it) {
     if (it==0) return 0;
     if (it <= 2) return 1;
     if (!memoized[it]) memoized[it] = fn(it - 1) + fn(it - 2) + fn(it - 3);
