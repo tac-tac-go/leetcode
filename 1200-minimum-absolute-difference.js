8d85eae3f64d7d48045c65885ba59f6cd8393e14
pypymi <dr.mizoguchi@gmail.com>
2024-05-22 11:15:51 +0900

    test

--- 1200-minimum-absolute-difference.js
+++ 1200-minimum-absolute-difference.js
@@ -2,19 +2,19 @@
  * @param {number[]} arr
  * @return {number[][]}
  */
-var minimumAbsDifference = function(arr) {
-    var arr_sort = [...arr]
-    arr_sort.sort((a,b) => a-b)
-    var result_min=[]
-    for(let i=0;i<arr_sort.length-1;i++){
-        result_min.push(Math.abs(arr_sort[i]-arr_sort[i+1]))
+const minimumAbsDifference = function (arr) {
+  let arr_sort = [...arr];
+  arr_sort.sort((a, b) => a - b);
+  let result_min = [];
+  for (let i = 0; i < arr_sort.length - 1; i++) {
+    result_min.push(Math.abs(arr_sort[i] - arr_sort[i + 1]));
+  }
+  let min_val = result_min.reduce((a, b) => Math.min(a, b));
+  let result = [];
+  for (let i = 0; i < arr_sort.length - 1; i++) {
+    if (Math.abs(arr_sort[i + 1] - arr_sort[i]) == min_val) {
+      result.push([arr_sort[i], arr_sort[i + 1]]);
     }
-    var min_val = result_min.reduce((a,b) => Math.min(a,b))
-    var result=[];
-    for(let i=0;i<arr_sort.length-1;i++){
-        if(Math.abs(arr_sort[i+1]-arr_sort[i])==min_val){
-            result.push([arr_sort[i],arr_sort[i+1]])
-        }
-    }
-    return result
+  }
+  return result;
 };
